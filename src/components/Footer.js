12dd47a1727c734f381f053f4966808eae5dea1e
Ambesh Mishra <ambesh@Ambeshs-MacBook-Air.local>
2024-10-30 20:26:59 +0530

    updated feature

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export const Footer = () => {
 
         <footer className="bg-white shadow  dark:bg-gray-800">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-              <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link to="https://flowbite.com/" className="hover:underline">Flowbite™</Link>. All Rights Reserved.
+              <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <Link to="https://flowbite.com/" className="hover:underline">CinemaTV™</Link>. All Rights Reserved.
             </span>
             <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li>
